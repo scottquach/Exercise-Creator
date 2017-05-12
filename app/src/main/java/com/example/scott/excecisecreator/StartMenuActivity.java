b8e3b26e6e6c1088885e310f9e650d499280177d
scottquach <you@example.com>
2017-05-11 19:12:45 -0700

    initialize layouts in MenuActivity

--- app/src/main/java/com/example/scott/excecisecreator/StartMenuActivity.java
+++ app/src/main/java/com/example/scott/excecisecreator/StartMenuActivity.java
@@ -1,20 +1,36 @@
 package com.example.scott.excecisecreator;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class StartMenuActivity extends AppCompatActivity {
+
+    private ConstraintLayout menuLayout;
+    private ConstraintSet originalConstraint = new ConstraintSet();
+
+    private CardView loadCard;
+    private CardView createCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+
+        //initialize
+        menuLayout = (ConstraintLayout) findViewById(R.id.StartMenuLayout);
+        originalConstraint.clone(menuLayout);
+        loadCard = (CardView) findViewById(R.id.loadExerciseCard);
+        createCard = (CardView) findViewById(R.id.createExerciseCard);
     }
 
     /*Start morph animation for loading
     an exercise
      */
     private void loadCardMorphAnim(){
+
 
     }
 
