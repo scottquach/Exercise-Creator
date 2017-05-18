5753ec8fb55b0a985c60c2e3e8bbaea17135fa63
Scott Quach <scottqglobal@gmail.com>
2017-05-18 13:44:37 -0700

    began implementing database

--- app/src/main/java/com/example/scott/excecisecreator/DataBaseHelper.java
+++ app/src/main/java/com/example/scott/excecisecreator/DataBaseHelper.java
@@ -1,8 +1,34 @@
 package com.example.scott.excecisecreator;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+
 /**
  * Created by scott on 5/18/2017.
  */
 
-public class DataBaseHelper {
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "exercises.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void createNewExercise(String name){
+    }
 }
