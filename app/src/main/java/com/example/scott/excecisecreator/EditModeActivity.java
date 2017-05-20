d91dfb221c043a9aaedbbabc06f87767f704d994
scottquach <you@example.com>
2017-05-20 08:36:29 -0700

    implementation of fab and recyclerView

--- app/src/main/java/com/example/scott/excecisecreator/EditModeActivity.java
+++ app/src/main/java/com/example/scott/excecisecreator/EditModeActivity.java
@@ -1,19 +1,37 @@
 package com.example.scott.excecisecreator;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EditModeActivity extends AppCompatActivity {
 
     private String exerciseName;
+    private RecyclerView recyclerview;
+    private ArrayList<String> entries = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_mode);
 
+        entries.add(0,"Hello");
+        entries.add(1, "GoodBye");
+        entries.add(2, "JK LOL");
+
+
+        recyclerview = (RecyclerView) findViewById(R.id.editExerciseRecycleView);
+
+        //Get exercise name to be edited
         Bundle extras = getIntent().getExtras();
         if (extras != null){
             exerciseName = extras.getString("name");
@@ -24,6 +42,65 @@ public class EditModeActivity extends AppCompatActivity {
         }
 
         getSupportActionBar().setTitle(exerciseName);
+
+        setUpRecycleView();
     }
 
+    private void setUpRecycleView(){
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(layoutManager);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(entries, this);
+        recyclerview.setAdapter(adapter);
+    }
+
+    private void loadData(){}
+
+
+    private void createTask(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New Task");
+
+        final EditText input = (EditText) new EditText(this);
+        input.setHint("Task Name");
+        builder.setView(input);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+
+    }
+
+    private void createPause(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create Pause");
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public void createPauseClicked(View view) {
+        createPause();
+    }
+
+    public void createTaskClicked(View view) {
+        createTask();
+    }
 }
