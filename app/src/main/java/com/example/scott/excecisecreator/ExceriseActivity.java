caffc06d4ffacace4171eef9e27f61d36f876231
Scott Quach <scottqglobal@gmail.com>
2017-06-12 09:41:10 -0700

    created the appropriate methods in ExerciseActivity to set up the recycle view

--- app/src/main/java/com/example/scott/excecisecreator/ExceriseActivity.java
+++ app/src/main/java/com/example/scott/excecisecreator/ExceriseActivity.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class ExceriseActivity extends AppCompatActivity {
 
     private DataBaseHelper db;
 
+    private RecyclerView recycleView;
+    private RecyclerAdapter adapter;
+
     private String exerciseName;
+
+    private ArrayList<String> entries = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class ExceriseActivity extends AppCompatActivity {
 
         //initialization
         db = new DataBaseHelper(this);
+        recycleView = (RecyclerView) findViewById(R.id.exerciseRecycleView);
 
         //get exercise name to load
         Bundle bundle = getIntent().getExtras();
@@ -39,10 +49,24 @@ public class ExceriseActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(exerciseName);
 
         loadData();
+        setUpRecycleView();
     }
 
+    /*Setup up layout manager and adapter for the
+    recycle view, uses the data set entries ArrayList
+     */
     private void setUpRecycleView(){
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recycleView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(entries, this);
+        recycleView.setAdapter(adapter);
+    }
 
+    /*Notifies the adapter to update
+    the recycleview with the edited content
+     */
+    private void updateRecycleView(){
+        adapter.notifyDataSetChanged();
     }
 
     private void loadData(){
