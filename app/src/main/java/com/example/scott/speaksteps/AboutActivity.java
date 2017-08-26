15ecda409e7c59b101cef52fd4092fa2311d5565
scott quach <scottqglobal@gmail.com>
2017-08-26 15:39:03 -0700

    Implemented about page

---
+++ app/src/main/java/com/example/scott/speaksteps/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.scott.speaksteps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+
+}
