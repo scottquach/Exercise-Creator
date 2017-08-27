a9b6353cc8eb45cd53ec2c69d9959f38343983eb
scott quach <scottqglobal@gmail.com>
2017-08-26 20:52:19 -0700

    Framework for a settings page

--- app/src/main/java/com/example/scott/speaksteps/SettingsActivity.java
+++ app/src/main/java/com/example/scott/speaksteps/SettingsActivity.java
@@ -2,25 +2,19 @@ package com.example.scott.speaksteps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 
-import mehdi.sakout.aboutpage.AboutPage;
-import mehdi.sakout.aboutpage.Element;
-
-public class AboutActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Instrumentation.getInstance().track(Instrumentation.TrackEvents.OPEN_ABOUT,
+        Instrumentation.getInstance().track(Instrumentation.TrackEvents.OPEN_SETTINGS,
                 Instrumentation.TrackParams.SUCCESS);
     }
 }
